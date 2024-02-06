52933bbf2d227a238d0f702985682fc4678d2fba
yns-jns <younessaitmbarek97@gmail.com>
2024-02-06 21:50:27 +0100

    commited

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="max-w-4xl mx-auto text-center h-screen flex flex-col justify-center">
+      <h1 className="text-6xl font-display mb-1">Love Code</h1>
+      <hr />
+      <h3 className="text-xl font-display mt-4">Vite template with Tailwind Css & Google font</h3>
+    </div>
+  );
+}
+
+export default App;
